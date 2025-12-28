e3529aada28ff4494e21c99695e00747250d4ffd
Akshaysarange <akshaysarange8@gmail.com>
2025-12-28 08:34:44 +0530

    main logic

---
+++ auth.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import User
+from app import db
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("auth.register"))
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration successful, Please login", "success")
+        return redirect(url_for("auth.login"))
+
+    return render_template('register.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            flash("Login successful", "success")
+            return redirect(url_for('tasks.view_tasks'))
+        else:
+            flash("Invalid username or password", 'danger')
+
+    return render_template('login.html')
+
+
+@auth_bp.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    flash('Logged out', 'info')
+
+    return redirect(url_for('auth.login'))
